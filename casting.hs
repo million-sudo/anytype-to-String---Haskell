12a3df823793616f0e24a5656843203ee3b43db1
million-sudo <75202265+million-sudo@users.noreply.github.com>
2021-02-21 19:35:26 +0900

    Create casting.hs

---
+++ casting.hs
@@ -0,0 +1,14 @@
+a :: Int
+a = 123
+b :: Float
+b = 456.789
+c :: Bool
+c = True
+d :: [Int]
+d = [1,2,3]
+
+main = do
+    print $ show a
+    print $ show b
+    print $ show c
+    print $ show d
