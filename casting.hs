a4d59cf293b081b71d32cc59dc1c1f2fb79567b8
million-sudo <75202265+million-sudo@users.noreply.github.com>
2021-02-21 19:40:33 +0900

    Update casting.hs

--- casting.hs
+++ casting.hs
@@ -1,3 +1,5 @@
+import Data.Typeable
+
 a :: Int
 a = 123
 b :: Float
@@ -8,7 +10,20 @@ d :: [Int]
 d = [1,2,3]
 
 main = do
+    putStrLn "Outputs:"
     print $ show a
     print $ show b
     print $ show c
     print $ show d
+
+    let a_ = show a
+    let b_ = show b
+    let c_ = show c
+    let d_ = show d
+
+    putStrLn "Variables Type:"
+    print $ typeOf a_
+    print $ typeOf b_
+    print $ typeOf c_
+    print $ typeOf d_
+
